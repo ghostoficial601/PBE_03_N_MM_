ad324ffc6e32b37e22fa7662a4cdec70fa43be4a
Pablo Matheus <pablo.pmlf@gmail.com>
2025-08-21 14:20:01 +0000

    adicao nova

--- pg1.php
+++ pg1.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>teste</title>
 </head>
 <body>
+    <h1>PHP</h1>
+    <?php
+    echo "<p>testetando....</p>"
+    ?>
     
 </body>
 </html>
